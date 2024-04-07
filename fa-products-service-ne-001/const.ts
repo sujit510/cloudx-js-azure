13aed9e1ba519420183c74e57b5c7f1f80c71d93
Sujit Singh <sujit_singh2@epam.com>
2024-04-08 00:09:55 +0530

    Added config service integration

--- fa-products-service-ne-001/const.ts
+++ fa-products-service-ne-001/const.ts
@@ -1,4 +1,6 @@
+import { Context } from "@azure/functions";
 import { Product } from "./types";
+import { AppConfigurationClient } from '@azure/app-configuration';
 
 export const products: Product[] = [{
   id: '001',
@@ -16,3 +18,14 @@ export const products: Product[] = [{
   description: 'Sharpen your TS skills with this book',
   price: 100,
 }];
+
+export const getConfigFromConfigService = async (ctx: Context) => {
+
+    // Create an App Config Client to interact with the service
+    const connection_string = process.env.AZURE_APP_CONFIG_CONNECTION_STRING;
+    const client = new AppConfigurationClient(connection_string);
+
+    // Retrieve a configuration key
+    const configs = await client.getConfigurationSetting({ key: 'MY_TEST_CONFIG_KEY' });
+    ctx.log('configs::', configs);
+}
